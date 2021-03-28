41a8a7c4539170c41ea29599e079474ad163d54b
Daniel151505 <daniel150520011@gmail.com>
2021-03-27 23:33:46 -0500

    Lista de items

--- covid/covidapp/views.py
+++ covid/covidapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def helloworldview(request):
     mylistitems = ['item1','item2','item3']
-
+    context = {'mylistitems' : mylistitems}
     return render(request,'helloworld.html', context)
