dde9afc620fd7fa2b60e7b326fe8eee5f4d6fac4
Daniel151505 <daniel150520011@gmail.com>
2021-03-27 23:42:05 -0500

    Agregando Api Covid-19

--- covid/covidapp/views.py
+++ covid/covidapp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+import requests
 
+url = "https://covid-193.p.rapidapi.com/statistics"
+
+headers = {
+    'x-rapidapi-key': "SIGN-UP-FOR-KEY",
+    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
 # Create your views here.
 def helloworldview(request):
-    mylistitems = ['item1','item2','item3']
-    context = {'mylistitems' : mylistitems}
+    
     return render(request,'helloworld.html', context)
