cec4e7e404fd6562a6ae466a399294f32cb133ff
Daniel151505 <daniel150520011@gmail.com>
2021-03-28 00:10:22 -0500

    Fix error

--- covid/covidapp/views.py
+++ covid/covidapp/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 import requests
+import json
 
 url = "https://covid-193.p.rapidapi.com/statistics"
 
 headers = {
-    'x-rapidapi-key': "SIGN-UP-FOR-KEY",
+    'x-rapidapi-key': "c01a2b41a5msh2047f72fbaa80dcp18d7a6jsne51475daddbe",
     'x-rapidapi-host': "covid-193.p.rapidapi.com"
     }
 
-response = requests.request("GET", url, headers=headers)
+response = requests.request("GET", url, headers=headers).json()
 
-print(response.text)
+
 # Create your views here.
 def helloworldview(request):
-    
+    context = {'response' : response}
     return render(request,'helloworld.html', context)
